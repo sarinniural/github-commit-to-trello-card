2b8f77fccff23ce4d5802d1d7476fb760bc5ccd3
Sarin Regmi <sarin@niural.com>
2022-11-02 16:58:15 -0500

    Support for branch names

--- dist/index.js
+++ dist/index.js
@@ -9084,7 +9084,7 @@ function getCardNumbers(message) {
   console.log(`getCardNumber(${message})`);
   console.log(`Trello ID match pattern ${trelloCardIdPattern}`)
   let ids = message && message.length > 0 ? message.replace(regexPullRequest, "").match(new RegExp(`${trelloCardIdPattern}\\d+`, 'g')) : [];
-  return ids && ids.length > 0 ? [...new Set(ids.map((x) => {return x.replace(trelloCardIdPattern, '');}))] : null;
+  return ids && ids.length > 0 ? ids[ids.length-1].replace(trelloCardIdPattern, '') : null;
 }
 
 function getAllCardNumbers(message, branch) {
